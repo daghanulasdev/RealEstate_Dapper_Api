a56f06674b3969e1bde28e6d90f923dea9221904
pxr <pxr@DESKTOP-BIP5GPD>
2023-08-04 14:07:15 +0300

    Add Dapper

---
+++ RealEstate_Dapper_Api/Dtos/UpdateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace RealEstate_Dapper_Api.Dtos
+{
+    public class UpdateCategoryDto
+    {
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.Dtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+
+    }
+}
